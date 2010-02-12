04c6917e1aeedc98d210b1f39acc39f2a57bd57a
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-02-12 11:32:21 -0200

    improved search example.

--- examples.py
+++ examples.py
@@ -1,14 +1,21 @@
 from meme import Meme
 
-# auth
-#MemeApi.authenticate(user='gchapiewski', passwd='mypasswd')
-
 # --> examples - posts
 print '========== Popular memes =========='
 print Meme.Posts.popular()
 
 print '========== Popular memes from Brazil =========='
 print Meme.Posts.popular(locale='pt')
+
+print '========== Sample search =========='
+posts = Meme.Posts.search('meme rocks')
+print posts
+
+print '---------- Results for "meme rocks" ----------'
+for post in posts:
+    print 'Content: %s' % post.content
+    print 'Caption: %s' % post.caption
+    print '----------------------------------------------'
 
 # --> examples - memes
 meme = Meme.get(name='guilherme_chapiewski')
@@ -24,6 +31,6 @@ print meme.following()
 print '========== 50 Memes following guilherme_chapiewski Meme =========='
 print meme.following(count=50)
 
-print '========== Sample search =========='
-posts = Meme.Posts.search('meme rocks')
-print posts
+# auth
+#MemeApi.authenticate(user='gchapiewski', passwd='mypasswd')
+
