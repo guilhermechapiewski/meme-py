70e38081e5b75d760dd045a340713bd3fcad62a5
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-03-10 19:17:41 -0300

    Link to documentation on PyPI.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     packages = ["meme"],
     author = "Guilherme Chapiewski",
     author_email = "guilherme.chapiewski@gmail.com",
-    description = "Yahoo! Meme client API.",
+    description = "Yahoo! Meme (http://meme.yahoo.com) client API.",
     license = "Apache License 2.0",
     keywords = "yahoo meme client api",
     url = "http://github.com/guilhermechapiewski/meme-py/",
-    long_description = "Python client API to consume Yahoo! Meme's (http://meme.yahoo.com) webservices on YQL (http://developer.yahoo.com/yql).",
+    long_description = "Python client API to consume Yahoo! Meme's (http://meme.yahoo.com) webservices on YQL (http://developer.yahoo.com/yql). Please check the documentation on Github: http://github.com/guilhermechapiewski/meme-py/blob/master/README.textile",
     install_requires=["yql >= 0.3"],
 )
