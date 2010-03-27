51c19b8ec8d21f4fa0ce13e9968336ab95ea250b
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-03-27 17:05:09 -0300

    Preparing for release 0.3.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "meme-py",
-    version = '0.2.1',
+    version = '0.3.0',
     packages = ["meme"],
     author = "Guilherme Chapiewski",
     author_email = "guilherme.chapiewski@gmail.com",
